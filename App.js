9f9d4ec2bad7557fe40bc0109d867982b7da11a6
Your Name <samsulr99@gmail.com>
2022-09-08 10:24:41 +0700

    prepare project for axios

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import * as React from "react";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 //Import Screen
-import Posts from "./src/screens/Posts";
-import DetailPost from "./src/screens/DetailPost";
+import Posts from './src/screens/Posts';
+import DetailPost from './src/screens/DetailPost';
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Posts" component={Posts} />
-        <Stack.Screen name="DetailPost" component={DetailPost} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default function App(){
+  return(
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Posts" component={Posts} />
+       <Stack.Screen name="DetailPost" component={DetailPost} />
+     </Stack.Navigator>
+   </NavigationContainer> 
+  )
 }
--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   View,
   Text,
@@ -10,69 +10,25 @@ import {
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
-import axios from "axios";
 
 const PostDetail = (props) => {
   //init Props
-  const title = props.route.params.title;
-  const body = props.route.params.body;
-  const id = props.route.params.id;
 
   //Init State
-  const [comments, setComments] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   // Create LifeCycle
-  useEffect(() => {
-    //Function Exception
-    getComment();
-  }, []);
+  //Function Exception
 
   // Create Function to fetch
-  const getComment = () => {
-    setIsLoading(true);
-    axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-      .then((res) => {
-        setComments(res.data);
-        setIsLoading(false);
-      })
-      .catch(() => {
-        alert("Error Get Comments");
-        setIsLoading(false);
-      });
-  };
 
   //   Create Component List
-  const _renderItem = ({ item }) => {
-    return (
-      <ListItem key={item.id.toString()} bottomDivider>
-        <ListItem.Content>
-          <ListItem.Title numberOfLines={1}>{item.email}</ListItem.Title>
-          <ListItem.Subtitle
-            numberOfLines={2}
-          >{`${item.name} - ${item.body}`}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-    );
-  };
-
   return (
     <View style={style.container}>
-      {/* Render Component List */}
       <Text h2 style={{ fontWeight: "bold" }}>
-        {title}
+        This Is Post Detail
       </Text>
-      <Text style={{ marginTop: 20 }}>{body}</Text>
-      <Text style={{ marginTop: 20, color: "grey" }}>Comments</Text>
-      <FlatList
-        data={comments}
-        renderItem={_renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        refreshControl={
-          <RefreshControl refreshing={isLoading} onRefresh={getComment} />
-        }
-      />
+      {/* Implement Axios Here */}
+      {/* Render Component List */}
     </View>
   );
 };
