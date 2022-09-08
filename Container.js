958488729ae0c48739bfaea6f44a20dc29cb8ad5
Your Name <samsulr99@gmail.com>
2022-09-08 10:07:14 +0700

    stack navigation

--- Container.js
+++ Container.js
@@ -1,13 +1,10 @@
 import * as React from "react";
 
 //Import Navigation Container
-import { NavigationContainer } from "@react-navigation/native";
 
 // Import Stack Navigation
-import { createStackNavigator } from "@react-navigation/stack";
 
 // Import Theme Native Base
-import { useTheme } from "native-base";
 
 // Import Screen
 import FormNativeBase from "./src/screens/formNativeBase";
@@ -15,37 +12,8 @@ import Hello from "./src/screens/hello";
 import IncDec from "./src/screens/incDec";
 
 // Create Stack Navigation
-const Stack = createStackNavigator();
 
 export default function Container() {
   // Init Theme
-  const theme = useTheme();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerMode: "screen",
-          headerTintColor: "white",
-          headerStyle: { backgroundColor: theme.colors.primary["300"] },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={Hello}
-          options={{
-            title: "Hello Screen",
-          }}
-        />
-        <Stack.Screen
-          name="IncDec"
-          component={IncDec}
-          options={{
-            title: "Increment Decrement",
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return; // Refactor Code Navigation
 }
